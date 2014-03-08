aba64088404e6f1d1f7914e9c02d78ea293ccf86
Michael Schuett <michaeljs1990@gmail.com>
2014-03-07 23:01:35 -0600

    added bubble sort

--- sort/bubble.php
+++ sort/bubble.php
@@ -8,3 +8,19 @@
  */
 
 $sort = array(13, 3, 4, 5, 1, 6, 7, 0, 1, 5, 8, 10, 12);
+
+$sorting = true;
+
+while($sorting){
+	$sorting = false;
+	for($x = 0; $x < count($sort) - 1; $x++){
+		if($sort[$x] > $sort[$x + 1]){
+			$temp = $sort[$x];
+			$sort[$x] = $sort[$x + 1];
+			$sort[$x + 1] = $temp;
+			$sorting = true; 
+		}
+	}
+}
+
+var_dump($sort);
